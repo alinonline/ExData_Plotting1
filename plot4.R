f2e9755032b38b92c4329f78e63295d90b8eeda3
alinonline <alin@acxiom.com>
2015-04-12 23:56:01 +1000

    my second submit

--- plot4.R
+++ plot4.R
@@ -6,7 +6,7 @@ png(file = "plot4.png", width = 480, height = 480)
 
 par(mfrow = c(2, 2), cex = 0.8, mar = c(4,4,2,2) )
 
-plot(hpc_use$DateTime, hpc_use$Global_active_power, cex = .2, xlab='', ylab='Glbal Active Power (kilowatts)', type="l")
+plot(hpc_use$DateTime, hpc_use$Global_active_power, cex = .2, xlab='', ylab='Glbal Active Power', type="l")
 
 plot(hpc_use$DateTime, hpc_use$Voltage, xlab='datetime', ylab='Voltage', type="l")
 
@@ -14,7 +14,7 @@ plot(hpc_use$DateTime,hpc_use$Sub_metering_1, xlab='', ylab='Energy sub metering
 points(hpc_use$DateTime, hpc_use$Sub_metering_1, col = "black" , type="l" )
 points(hpc_use$DateTime, hpc_use$Sub_metering_2, col = "red" , type="l" )
 points(hpc_use$DateTime, hpc_use$Sub_metering_3, col = "blue" , type="l" )
-legend("topright", cex = .8, lty=1, col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", cex = .9, lty=1,  bty="n", col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 plot(hpc_use$DateTime, hpc_use$Global_reactive_power, xlab='datetime', ylab='Global_reactive_power', type="l")
 
